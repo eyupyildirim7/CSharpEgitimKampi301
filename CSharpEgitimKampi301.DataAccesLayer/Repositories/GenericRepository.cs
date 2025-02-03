b794847dba7014c1eb0b800c652ae006042fd4ba
Eyüp Mustafa YILDIRIM <eyupyldrm07@gmail.com>
2025-02-03 19:37:23 +0300

    EntıtyState Komutları

---
+++ CSharpEgitimKampi301.DataAccesLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using CSharpEgitimKampi301.DataAccesLayer.Abstract;
+using CSharpEgitimKampi301.DataAccesLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.DataAccesLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        KampContext context=new KampContext();
+        private readonly DbSet<T> _object ;
+
+        public GenericRepository()
+        {
+            _object =context.Set<T>();
+        }
+        public void Delete(T entity)
+        {
+            var deletedEntity=context.Entry(entity);
+            deletedEntity.State = EntityState.Detached;
+        }
+
+        public List<T> GetAll()
+        {
+            return _object.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return _object.Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            var addedEntity=context.Entry(entity);
+            addedEntity.State = EntityState.Added;
+        }
+
+        public void Update(T entity)
+        {
+            var updatedEntity=context.Entry(entity);
+            updatedEntity.State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+}
